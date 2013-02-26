c519395a2e2ec26bd9ea9e8e9328214216971590
larssorenson@outlook.com <larssorenson@outlook.com>
2013-02-26 14:41:23 -0500

    Added file for the Rooms API. Added skeleton functions to be later implemented.

---
+++ web/application/controllers/api/rooms.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Rooms
+{
+	public function join_room($room_id, $user_id)
+	{
+		//Add a user to a room
+	}
+	public function leave_room($room_id, $user_id)
+	{
+		//Remove a user from a room
+	}
+	public function add_room($room_id)
+	{
+		//Add a room
+	}
+}
