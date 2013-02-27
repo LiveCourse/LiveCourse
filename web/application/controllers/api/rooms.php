04e3ab4974cac69d1bac63a58a716541a3b5134a
Hayden McAfee <hayden@outlook.com>
2013-02-26 21:29:30 -0500

    Fixed first API committed by Lars for work item #75 This is a RESTFUL class. Each method should fall under a RESTful HTTP state (GET, PUT, POST, or DELETE) See http://en.wikipedia.org/wiki/Representational_state_transfer#RESTful_web_services See also https://github.com/philsturgeon/codeigniter-restserver/blob/master/README.md#requirements NOTE: POST Updates a resource. PUT CREATES or COMPLETELY replaces what is already there.

--- web/application/controllers/api/rooms.php
+++ web/application/controllers/api/rooms.php
@@ -1,17 +1,34 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require(APPPATH.'libraries/REST_Controller.php');
 
-class Rooms
+/*
+ * This is a RESTFUL class. Each method should fall under a RESTful HTTP state
+ * (GET, PUT, POST, or DELETE)
+ * See http://en.wikipedia.org/wiki/Representational_state_transfer#RESTful_web_services
+ * See also https://github.com/philsturgeon/codeigniter-restserver/blob/master/README.md#requirements
+ * NOTE: POST Updates a resource. PUT CREATES or COMPLETELY replaces what is already there.
+ */
+ 
+class Rooms extends REST_Controller
 {
-	public function join_room($room_id, $user_id)
+	// Index command is run when no command is specified.
+	public function index_get()
+	{
+		//Return a list of all subscribed rooms, unless a room_id is specified
+		$room_id = $this->get('id'); // GET param
+	}
+	public function join_post()
 	{
 		//Add a user to a room
+		$room_id = $this->post('id'); // GET param
 	}
-	public function leave_room($room_id, $user_id)
+	public function leave_post()
 	{
 		//Remove a user from a room
+		$room_id = $this->post('id'); // GET param
 	}
-	public function add_room($room_id)
+	public function add_put()
 	{
-		//Add a room
+		//Gather information (class name, location, etc)
 	}
 }
