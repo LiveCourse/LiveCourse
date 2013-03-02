e6f34861eda661115bb7105b3a38e9bbd62151e8
Darren <zades@outlook.com>
2013-03-02 01:34:54 -0500

    #88 Added tabs to the UI

--- android/src/net/livecourse/MainActivity.java
+++ android/src/net/livecourse/MainActivity.java
@@ -1,17 +1,62 @@
 package net.livecourse;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.*;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.Toast;
 
 public class MainActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final ActionBar actionBar = getSupportActionBar();
+		
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		ActionBar.TabListener tabListener = new ActionBar.TabListener()
+		{
+
+			@Override
+			public void onTabSelected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTabReselected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		
+		//Adds three tabs
+		/*
+		for(int i = 0; i<3;i++)
+		{
+			actionBar.addTab(actionBar.newTab().setText("Tab " + (i+1)).setTabListener(tabListener));
+		}
+		*/
+		actionBar.addTab(actionBar.newTab().setText("Class List").setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText("Chat").setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText("Participants").setTabListener(tabListener));
+
+
+	
 	}
 	
 	@Override
