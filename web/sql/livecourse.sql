036c4e1530a599f5dd3109704bdf967605e41e4f
Hayden McAfee <hayden@outlook.com>
2013-03-11 21:25:53 -0500

    Modified SQL Schema to add more flexibility / relational models. Also makes things incredibly complicated. Model / API updates to follow.

--- web/sql/livecourse.sql
+++ web/sql/livecourse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.6
+-- version 3.5.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 04, 2013 at 03:12 AM
--- Server version: 5.5.30
--- PHP Version: 5.4.11
+-- Generation Time: Mar 12, 2013 at 02:24 AM
+-- Server version: 5.5.28-log
+-- PHP Version: 5.3.15
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,21 +17,36 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `hayden_livecourse`
+-- Database: `livecourse`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lc_rooms`
+-- Table structure for table `lc_buildings`
 --
 
-CREATE TABLE IF NOT EXISTS `lc_rooms` (
+CREATE TABLE IF NOT EXISTS `lc_buildings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
+  `institution_id` int(11) NOT NULL COMMENT 'ID of institution where this building resides',
+  `name` varchar(512) NOT NULL COMMENT 'Name of the building',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lc_chats`
+--
+
+CREATE TABLE IF NOT EXISTS `lc_chats` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Room ID, primary index',
   `id_string` varchar(12) NOT NULL COMMENT 'Randomized string for identifying the room to the client',
+  `subject_id` int(11) NOT NULL COMMENT 'ID of subject this room belongs to',
+  `course_number` smallint(6) NOT NULL COMMENT 'Course number',
   `name` varchar(100) NOT NULL COMMENT 'Name of the room / class',
-  `building` varchar(100) NOT NULL COMMENT 'Name of the building this class takes place in',
-  `room` varchar(100) NOT NULL COMMENT 'Room that the class takes place in',
+  `institution_id` int(11) NOT NULL COMMENT 'identifier of the institution this room is offered at',
+  `room_id` int(11) NOT NULL COMMENT 'room identifier from lc_rooms',
   `start_time` int(5) NOT NULL COMMENT 'Time that the class starts at in format MINUTES SINCE MIDNIGHT UTC',
   `end_time` int(5) NOT NULL COMMENT 'Time that the class ends at in format MINUTES SINCE MIDNIGHT UTC',
   `start_date` date NOT NULL COMMENT 'Date that the class begins',
@@ -49,14 +64,54 @@ CREATE TABLE IF NOT EXISTS `lc_rooms` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lc_room_participants`
+-- Table structure for table `lc_chat_participants`
 --
 
-CREATE TABLE IF NOT EXISTS `lc_room_participants` (
+CREATE TABLE IF NOT EXISTS `lc_chat_participants` (
   `room_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `permissions` int(11) NOT NULL DEFAULT '0' COMMENT '0 = default, 1 = admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lc_institutions`
+--
+
+CREATE TABLE IF NOT EXISTS `lc_institutions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identification Number',
+  `name` varchar(255) NOT NULL COMMENT 'Name of institution',
+  `zip` varchar(5) NOT NULL COMMENT 'Zip code of institution location',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lc_rooms`
+--
+
+CREATE TABLE IF NOT EXISTS `lc_rooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Room identifier',
+  `building_id` int(11) NOT NULL COMMENT 'ID of building this room is located in',
+  `room_number` varchar(64) NOT NULL COMMENT 'Room number in building',
+  `room_name` varchar(128) NOT NULL COMMENT 'Room name, alias',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lc_subjects`
+--
+
+CREATE TABLE IF NOT EXISTS `lc_subjects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
+  `name` varchar(256) NOT NULL COMMENT 'Name of subject',
+  `code` varchar(5) NOT NULL COMMENT 'Code (Computer science = CS, etc)',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
