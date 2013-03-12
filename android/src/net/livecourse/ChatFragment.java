8c779d28c4ec211429ea06e69065aded874647ca
Darren <zades@outlook.com>
2013-03-12 01:15:51 -0400

    #98 The chat fragment (which is the tab that display chat/messages and allows sending messages) is done as far as the UI goes.  If you press send, it will send messages.  Currently the messages are stored in a array, which will be changed to a cursor for the SQLite once we get that going.

--- android/src/net/livecourse/ChatFragment.java
+++ android/src/net/livecourse/ChatFragment.java
@@ -1,65 +1,119 @@
 package net.livecourse;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.LinearLayout.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
-public class ChatFragment extends SherlockFragment{
-	
+public class ChatFragment extends SherlockFragment {
+
 	private static final String KEY_CONTENT = "TestFragment:Content";
 
-    public static ChatFragment newInstance(String content) {
-        ChatFragment fragment = new ChatFragment();
+	/**
+	 * This section declares all the views that this fragment handles Could
+	 * probably do with better names lol
+	 */
+	private View chatView;
+	private ListView messageListView;
+	private Button sendButtonView;
+	private EditText sendMessageEditTextView;
 
-        StringBuilder builder = new StringBuilder();
+	/**
+	 * This is the adapter used for the message list.
+	 * It is incomplete.
+	 */
+	private ChatListAdapter<String> adapter;
+	
+	/**
+	 * Temporary list used to populate the message list
+	 */
+	String[] array = {"a","b","c","d","e","f","g"}; 
+	ArrayList<String> messages;
+	
+	public static ChatFragment newInstance(String content) {
+		ChatFragment fragment = new ChatFragment();
+		StringBuilder builder = new StringBuilder();
+		builder.append("This is the chat fragment");
+		fragment.mContent = builder.toString();
 
-        builder.append("This is the chat fragment");
+		return fragment;
+	}
 
-        fragment.mContent = builder.toString();
+	private String mContent = "???";
 
-        return fragment;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    private String mContent = "???";
+		if ((savedInstanceState != null)
+				&& savedInstanceState.containsKey(KEY_CONTENT)) {
+			mContent = savedInstanceState.getString(KEY_CONTENT);
+		}
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+	{
+		/**
+		 * Initialize the temporary list
+		 */
+		messages = new ArrayList<String>();
+		messages.addAll(Arrays.asList(array));
+		
+		/**
+		 * Connects the views to their XML equivalent
+		 */
+		chatView = inflater.inflate(R.layout.chat_layout, container, false);
+		messageListView = (ListView) chatView.findViewById(R.id.message_list_view);
+		sendButtonView = (Button) chatView.findViewById(R.id.send_button_view);
+		sendMessageEditTextView = (EditText) chatView.findViewById(R.id.send_message_edit_text_view);
+		
+		/**
+		 * Adds the adapter to the list and sends the temporary list to it
+		 */
+		adapter = new ChatListAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1,messages);
+		messageListView.setAdapter(adapter);
+		
 
-        if ((savedInstanceState != null) && savedInstanceState.containsKey(KEY_CONTENT)) {
-            mContent = savedInstanceState.getString(KEY_CONTENT);
-        }
-    }
+		/**
+		 * Handels the send button. The button itself will most likely be
+		 * replaced with a cooler looking image And there probably is a better
+		 * way to implement a listener than this but I will need to look that up
+		 * later
+		 */
+		sendButtonView.setOnClickListener(new View.OnClickListener() {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        /*TextView text = new TextView(getActivity());
-        text.setGravity(Gravity.CENTER);
-        text.setText(mContent);
-        text.setTextSize(20 * getResources().getDisplayMetrics().density);
-        text.setPadding(20, 20, 20, 20);
+			@Override
+			public void onClick(View v) {
+				
+				if(!sendMessageEditTextView.getText().equals(""))
+				{
+					/**
+					 * Update the list and sends update the adapter, then change
+					 * EditText back to blank
+					 */
+					messages.add(sendMessageEditTextView.getText().toString());
+			        adapter.notifyDataSetChanged();
+					sendMessageEditTextView.setText("");
+				}
+			}
+		});
 
-        LinearLayout layout = new LinearLayout(getActivity());
-        layout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-        layout.setGravity(Gravity.CENTER);
-        layout.addView(text);
-        */
-    	
-    	return inflater.inflate(R.layout.chat_layout, container, false);
-        //return layout;
-    }
+		return chatView;
+	}
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(KEY_CONTENT, mContent);
-    }
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString(KEY_CONTENT, mContent);
+	}
 
 }
