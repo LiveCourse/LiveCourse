ed137a5105c9ab65ff2de6a272713571bb15b305
Hayden McAfee <hayden@outlook.com>
2013-03-28 21:26:36 -0400

    Test suite stuff.

---
+++ web/application/controllers/test.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('testclient');
+	}
+}
+
+/* End of file html5client.php */
+/* Location: ./application/controllers/html5client.php */
