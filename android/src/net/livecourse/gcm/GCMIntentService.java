e3cfad9688c922585c9b820f0340d13144089ced
lars <larssorenson@outlook.com>
2013-04-05 03:31:09 +0000

    Continued connecting the Google API page to the android project. See work item #130.

--- android/src/net/livecourse/gcm/GCMIntentService.java
+++ android/src/net/livecourse/gcm/GCMIntentService.java
@@ -1,45 +1,109 @@
 package net.livecourse.gcm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
 
 import com.google.android.gcm.GCMBaseIntentService;
+ 
+import static net.livecourse.gcm.GCMCommonUtilities.SENDER_ID;
+import static net.livecourse.gcm.GCMCommonUtilities.displayMessage;
 
 public class GCMIntentService extends GCMBaseIntentService
 {
 
+ 
+	private static final String TAG = "GCMIntentService";
+	
+	public GCMIntentService()
+	{
+	    super(SENDER_ID);
+	}
+    
 	@Override
 	protected void onError(Context arg0, String arg1) 
 	{
-		// TODO Auto-generated method stub
+		
+		Log.i(TAG, "Received error: " + errorId);
+		displayMessage(context, getString(R.string.gcm_error, errorId));
 		
 	}
 
 	@Override
 	protected void onMessage(Context arg0, Intent arg1) 
 	{
-		// TODO Auto-generated method stub
+	
+		Log.i(TAG, "Received message");
+		String message = intent.getExtras().getString("price");
+	 
+		displayMessage(context, message);
+		// notifies user
+		generateNotification(context, message);
 		
 	}
 
 	@Override
 	protected void onRegistered(Context arg0, String arg1) 
 	{
-		// TODO Auto-generated method stub
 		
+		Log.i(TAG, "Device registered: regId = " + registrationId);
+		displayMessage(context, "Your device registred with GCM");
+		Log.d("NAME", MainActivity.name);
+		GCMServerUtilities.register(context, MainActivity.name, MainActivity.email, registrationId);
+    
 	}
 
 	@Override
 	protected void onUnregistered(Context arg0, String arg1) 
 	{
-		// TODO Auto-generated method stub
 		
+		Log.i(TAG, "Device unregistered");
+		displayMessage(context, getString(R.string.gcm_unregistered));
+		GCMServerUtilities.unregister(context, registrationId);
+    
 	}
 	@Override
 	protected boolean onRecoverableError(Context context, String errorId)
 	{
-		// TODO Auto-generated method stub
-		return false;
+		
+		// log message
+		Log.i(TAG, "Received recoverable error: " + errorId);
+		displayMessage(context, getString(R.string.gcm_recoverable_error,
+			errorId));
+		return super.onRecoverableError(context, errorId);
+	
 	}
+	private static void generateNotification(Context context, String message)
+	{
+		
+		int icon = R.drawable.ic_launcher;
+		long when = System.currentTimeMillis();
+		NotificationManager notificationManager = (NotificationManager)
+			context.getSystemService(Context.NOTIFICATION_SERVICE);
+		Notification notification = new Notification(icon, message, when);
+	 
+		String title = context.getString(R.string.app_name);
+	 
+		Intent notificationIntent = new Intent(context, MainActivity.class);
+		// set intent so it does not start a new activity
+		notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+			Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		PendingIntent intent =
+			PendingIntent.getActivity(context, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(context, title, message, intent);
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+	 
+		// Play default notification sound
+		notification.defaults |= Notification.DEFAULT_SOUND;
+	 
+		// Vibrate if vibrate is enabled
+		notification.defaults |= Notification.DEFAULT_VIBRATE;
+		notificationManager.notify(0, notification);      
+ 
+    }
 
 }
