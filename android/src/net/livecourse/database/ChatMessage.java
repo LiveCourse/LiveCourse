ddb74ffffa06d1097e9da4e28cb2f9573be9cdbd
Darren Cheng <zades@outlook.com>
2013-04-04 22:22:32 -0400

    Added more documentation and comments

--- android/src/net/livecourse/database/ChatMessage.java
+++ android/src/net/livecourse/database/ChatMessage.java
@@ -1,5 +1,10 @@
 package net.livecourse.database;
 
+/**
+ * This is the object that holds the information for ChatMessages.
+ * 
+ * @author Darren Cheng
+ */
 public class ChatMessage 
 {	
 	private String chatId;
@@ -12,34 +17,44 @@ public class ChatMessage
 	{
 		return "Chat ID: "+ chatId +" Send Time: " + sendTime +	" MessageString: " + messageString + " email: " + email + " Display Name: " + displayName;
 	}
-	public String getChatId() {
+	public String getChatId() 
+	{
 		return chatId;
 	}
-	public void setChatId(String chatId) {
+	public void setChatId(String chatId) 
+	{
 		this.chatId = chatId;
 	}
-	public String getSendTime() {
+	public String getSendTime() 
+	{
 		return sendTime;
 	}
-	public void setSendTime(String sendTime) {
+	public void setSendTime(String sendTime) 
+	{
 		this.sendTime = sendTime;
 	}
-	public String getMessageString() {
+	public String getMessageString() 
+	{
 		return messageString;
 	}
-	public void setMessageString(String messageString) {
+	public void setMessageString(String messageString) 
+	{
 		this.messageString = messageString;
 	}
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 	}
-	public void setEmail(String email) {
+	public void setEmail(String email) 
+	{
 		this.email = email;
 	}
-	public String getDisplayName() {
+	public String getDisplayName() 
+	{
 		return displayName;
 	}
-	public void setDisplayName(String displayName) {
+	public void setDisplayName(String displayName) 
+	{
 		this.displayName = displayName;
 	}
 }
