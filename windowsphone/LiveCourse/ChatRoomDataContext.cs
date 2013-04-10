1ab4f8916464513a9c54885d9238b178ff24354c
Hayden <hayden@outlook.com>
2013-04-09 21:19:34 -0400

    Authentication and class listings!

---
+++ windowsphone/LiveCourse/ChatRoomDataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace LiveCourse
+{
+    class ChatRoomDataContext: DataContext
+    {
+        public ChatRoomDataContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+        public Table<MyChatRoom> ChatRooms
+        {
+            get
+            {
+                return this.GetTable<MyChatRoom>();
+            }   
+        }
+    }
+}
